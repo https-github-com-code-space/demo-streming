62c29265eec2326da9a4705f458e8d5087e567ba
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-06-07 23:01:27 +0600

    add

--- src/Pages/HomePage/VideoPlayer/VideoPlayer.jsx
+++ src/Pages/HomePage/VideoPlayer/VideoPlayer.jsx
@@ -3,32 +3,44 @@ import { MdClose } from "react-icons/md";
 import Modal from 'react-modal';
 import ReactPlayer from 'react-player';
 import './VideoPlayer.css'
-import img from './../../../assets/property/tham.jpeg'
+import img from './../../../assets/property/thamb.jpg'
 
 const videoData = [
     {
       id: 1,
       img: img,
-      title: 'Video 1',
-      url: 'https://youtu.be/0SCopio77gM',
+      title: 'something you own that is attached to a piece of land.',
+      url: 'https://youtu.be/y9j-BL5ocW8',
     },
     {
       id: 2,
       img: img,
-      title: 'Video 2',
-      url: 'https://youtu.be/CTF8LwhL9uE',
+      title: 'something you own that is attached to a piece of land.',
+      url: 'https://youtu.be/4jnzf1yj48M',
     },
     {
       id: 3,
       img: img,
-      title: 'Video 3',
-      url: 'https://youtu.be/y-X8mF_5PUw',
+      title: 'something you own that is attached to a piece of land.',
+      url: 'https://youtu.be/mgulkcaPMCY',
     },
     {
       id: 4,
       img: img,
-      title: 'Video 4',
-      url: 'https://youtu.be/CTF8LwhL9uE',
+      title: 'something you own that is attached to a piece of land.',
+      url: 'https://youtu.be/7EHnQ0VM4KY',
+    },
+    {
+      id: 5,
+      img: img,
+      title: 'something you own that is attached to a piece of land.',
+      url: 'https://youtu.be/7EHnQ0VM4KY',
+    },
+    {
+      id: 6,
+      img: img,
+      title: 'something you own that is attached to a piece of land.',
+      url: 'https://youtu.be/7EHnQ0VM4KY',
     },
     // Add more videos as needed
   ];
@@ -53,14 +65,14 @@ const VideoPlayer = () => {
       <h1 className='mb-20 text-4xl text-[#4b69bd] font-bold text-center'>Video Gallery</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
         {videoData.map((video) => (
-          <div key={video.id} className=" mx-auto" onClick={() => openModal(video)}>
+          <div key={video.id} className=" mx-auto shadow-lg shadow-[#4b69bd] rounded-lg px-2 py-2" onClick={() => openModal(video)}>
             <img className='cursor-pointer w-full' src={video.img} />
-            <h2 className='text-xl text-[#4b69bd] font-bold mt-2'>{video.title}</h2>
+            <h2 className='text-xl text-[#4b69bd] font-bold mt-1 px-2'>{video.title}</h2>
           </div>
         ))}
       </div>
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
-        <button className='ml-auto block text-3xl mb-10' onClick={closeModal}><MdClose></MdClose></button>
+      <Modal className='m-20 bg-white border-none outline-none' isOpen={modalIsOpen} onRequestClose={closeModal}>
+        <button className='ml-auto block text-3xl mb-1' onClick={closeModal}><MdClose></MdClose></button>
         <ReactPlayer url={currentVideo?.url} controls volume  width='100%' height='70vh' />
       </Modal>
     </div>
